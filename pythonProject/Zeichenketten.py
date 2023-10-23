30834af97758d6ed1791dd540a3b5fb31845cd3a
Marco Schneider <marco.schn@outlook.com>
2023-10-23 15:25:37 +0200

    Zeichen operationen

---
+++ pythonProject/Zeichenketten.py
@@ -0,0 +1,9 @@
+text = "Always look on the bright side of life - WISTLE"
+print(text.lower())
+print(len(text))
+print(text[:-8])
+longtext = text[:10] + text[10:18] + text[18:33] + text[33:]
+print(longtext)
+print(longtext[5:19])
+
+
