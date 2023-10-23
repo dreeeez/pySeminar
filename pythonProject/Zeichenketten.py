ede9d93e6a5b447f580a828f9cb8596a942f058d
Marco Schneider <marco.schn@outlook.com>
2023-10-23 15:47:28 +0200

    cool

--- pythonProject/Zeichenketten.py
+++ pythonProject/Zeichenketten.py
@@ -2,8 +2,7 @@ text = "Always look on the bright side of life - WISTLE"
 print(text.lower())
 print(len(text))
 print(text[:-8])
-longtext = text[:10] + text[10:18] + text[18:33] + text[33:]
-print(longtext)
-print(longtext[5:19])
+print((text + "\n") * 4)
+
 
 
